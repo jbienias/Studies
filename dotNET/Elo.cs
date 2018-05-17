732fad1a5bcb578202912ffe055788e58dd340d4
Jan Bienias <jan.bienias@hotmail.com>
2018-05-17 14:05:48 +0200

    simple test

---
+++ dotNET/Elo.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace MessageService
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+			Console.WriteLine("C#");
+			Console.WriteLine("C#");
+			Console.WriteLina("C#");
+			Console.WriteLine("C#");
+			Console.WriteLina("C#");
+			Console.WriteLine("C#");
+			Console.WriteLina("C#");
+			Console.WriteLina("C#");
+        }
+    }
+}
